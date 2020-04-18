a6a77ebf5ce15ce6ede78438ae604ddb2e10f235
Denis Gribanov <instance6@gmail.com>
2020-04-18 20:19:52 +0300

    copyright fix

--- benchmarks/mem_test.go
+++ benchmarks/mem_test.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2018-present unTill Pro, Ltd. and Contributors
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+ 
 package benchmarks
 
 import (
